9e03ff8e1cf71a4b68a3d21b62a345e038baead6
dickson <dkerobean@gmail.com>
2023-07-05 13:19:18 +0000

    login done

--- frontend/urls.py
+++ frontend/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('', views.index_page, name="index"), 
     path('shop/', views.shop_page, name="shop"), 
     path('about/', views.about_page, name="about"), 
-    path('contact/', views.contact_page, name="contact")
+    path('contact/', views.contact_page, name="contact"), 
+    
+    path('login/', views.user_login, name="login")
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
 
 def index_page(request):
     
@@ -20,3 +23,36 @@ def about_page(request):
 def contact_page(request):
     
     return render(request, 'frontend/ui/contact.html')
+
+
+
+""" AUTH """
+
+def user_login(request):
+    
+    if request.user.is_authenticated:
+        return redirect('index')
+    
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        try:
+            user = User.objects.get(username=username)
+        except UserNotFound:
+            messages.error(request,"Username or Password is incorrect")
+            return redirect('index')
+            
+        user = authenticate(request, username=username, password=password)
+        
+        if user is None:
+            messages.error(request, 'Username or Passwors incorrect')
+            return redirect('shop')
+        
+        login(request, user)
+        messages.success(request, 'Logged in')
+        return redirect('contact')
+        
+        
+    
+    return render(request, 'frontend/ui/index.html')
