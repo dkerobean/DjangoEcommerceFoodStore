20a4a998bebdaf93e8326ae2ef3930ec72a7a553
dickson <dkerobean@gmail.com>
2023-07-05 11:40:45 +0000

    UI done

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.index_page, name="index-page")
+    path('', views.index_page, name="index"), 
+    path('shop/', views.shop_page, name="shop"), 
+    path('about/', views.about_page, name="about"), 
+    path('contact/', views.contact_page, name="contact")
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -3,4 +3,20 @@ from django.shortcuts import render
 def index_page(request):
     
     
-    return render(request, 'frontend/index.html')
+    return render(request, 'frontend/ui/index.html')
+
+
+def shop_page(request):
+    
+    
+    return render(request, 'frontend/ui/shop.html')
+
+
+def about_page(request):
+    
+    return render(request, 'frontend/ui/about.html')
+
+
+def contact_page(request):
+    
+    return render(request, 'frontend/ui/contact.html')
