fb8318984c0d19b5a02a1bf19a2abda9521ad13a
dickson <dkerobean@gmail.com>
2023-07-06 11:46:43 +0000

    signals to automatically create user profiles done

---
+++ frontend/signals.py
@@ -0,0 +1,31 @@
+from .models import UserProfile
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+
+
+@reciever(post_save, sender=User)
+def create_profile(sender, created, instance, **kwargs):
+    
+    if created:
+        user = instance
+        profile = UserProfile.create(
+            user = user,
+            username = user.username,
+            email = user.email
+            
+        )
+        
+@reciever(post_delete, sender=User)        
+def delete_profile(sender, instance, **kwargs):
+    
+    user = instance.user
+    user.delete()
+    
+    
+        
+        
+        
+
+        
+        
