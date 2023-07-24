f96aa15720c9a5ac0e84f9d82e0a47d1e1042015
dickson <dkerobean@gmail.com>
2023-07-24 11:44:27 +0000

    added review model

--- frontend/admin.py
+++ frontend/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import UserProfile, Category, Product, Order, Address
+from .models import UserProfile, Category, Product, Order, Address, Review
 
 admin.site.register(UserProfile)
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Order)
 admin.site.register(Address)
+admin.site.register(Review)
